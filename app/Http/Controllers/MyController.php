33270f34ef83efd1f67ba81d29770cfd1425ca1f
Nguyen Vu Linh <nvlinh1309@gmail.com>
2019-06-18 14:42:03 +0700

    add MyController.php file

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
-    //
+	//Controller
+    public function getController($i,$j){
+    	return $tong=$i+$j;
+    }
+    //View
+    public function getView(){
+    	//$data['user']=['My View 1','My View 2','My View 3'];
+    	$data1['number']=1;
+    	//return view('MyView',$data);
+    	return view('MyView',$data1);
+    }
 }
